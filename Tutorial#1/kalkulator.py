f6166b540e78ebb4a45433acba36a74e6cf902cc
Szkola <Szkola@gmail.com>
2022-10-20 16:27:43 +0200

    sdfsfdsf

---
+++ Tutorial#1/kalkulator.py
@@ -0,0 +1,19 @@
+X = (float(input("Podaj X: ")))
+Sp = input("Podaj +,-,*,/: ")
+Y = (float(input("Podaj Y: ")))
+
+if Sp == "+":
+    Z = X + Y
+    print (Z)
+
+if Sp == "-":
+    Z = X - Y
+    print (Z)
+
+if Sp == "*":
+    Z = X * Y
+    print (Z)
+
+if Sp == "/":
+    Z = X / Y
+    print (Z)
