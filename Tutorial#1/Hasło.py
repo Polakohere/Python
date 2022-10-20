cd4d1a6d1cab9a94429d951365cc2982470355ef
Szkola <Szkola@gmail.com>
2022-10-20 14:53:10 +0200

    adasdasd

---
+++ Tutorial#1/Hasło.py
@@ -0,0 +1,10 @@
+haslo = "janek15"
+haslo = input("Wpisz haslo: ")
+while haslo != "janek15":
+    print ("Żle brek")
+    haslo = input("Wpisz haslo: ")
+else:
+    print ("Dobrze brek")
+
+
+
---
+++ Tutorial#1/Termometr.py
@@ -0,0 +1,9 @@
+temperatura = (int(input("Podaj ile jest stopni: ")))
+# pogoda = input("Jaką mamy dziś pogodę")
+if temperatura > 0 and temperatura < 30:
+    print("Pogoda jest gitem")
+elif temperatura > 30:
+    print("Jest straszny upał")
+    print("i pogoda jest paskudna")
+else:
+    print("i pogoda jest paskudna")
