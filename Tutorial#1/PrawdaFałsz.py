29f082292d4a3efece632a5eda3a8fb8e26fae25
Polakohere <kpiotrowski240@gmail.com>
2022-10-23 16:12:25 +0200

    adasdas

---
+++ Tutorial#1/PrawdaFałsz.py
@@ -0,0 +1,87 @@
+
+Pyt1 = "Japonia leży w Azji"
+Pyt2 = "Europa jest największym kontynentem"
+Pyt3 = "Na naszej planecie mamy 5 kontynentów"
+Pyt4 = "Największa pustynia to Sahara"
+Pyt5 = "Największa rzeka w polsce to Wisła"
+Pyt6 = "W naszym kraju mamy ruch lewo-stronny"
+# # Pyt7 = ("Japonia leży w Azji")
+# # Pyt8 = ("Japonia leży w Azji")
+# # Pyt9 = ("Japonia leży w Azji")
+# # Pyt0 = ("Japonia leży w Azji")
+Odp1 = "Prawda"
+Odp2 = "Fałsz"
+Odp3 = "Fałsz"
+Odp4 = "Prawda"
+Odp5 = "Prawda"
+Odp6 = "Fałsz"
+# # Odp7 = (Prawda)
+# # Odp8 = (Prawda)
+# # Odp9 = (Prawda)
+# # Odp0 = (Prawda)
+
+
+print (Pyt1)
+Odp1 = input("Prawda czy fałsz: ")
+if Odp1 == "Prawda":
+    print("Dobra odpowiedź!")
+else:
+    print("Zła odpowiedź!")
+print()
+
+print (Pyt2)
+Odp2 = input("Prawda czy fałsz: ")
+if Odp2 == "Fałsz":
+    print("Dobra odpowiedź!")
+else:
+    print("Zła odpowiedź!")
+print()
+
+print (Pyt3)
+Odp3 = input("Prawda czy fałsz: ")
+if Odp3 == "Fałsz":
+    print("Dobra odpowiedź!")
+else:
+    print("Zła odpowiedź!")
+print()
+
+print (Pyt4)
+Odp4 = input("Prawda czy fałsz: ")
+if Odp4 == "Prawda":
+    print("Dobra odpowiedź!")
+else:
+    print("Zła odpowiedź!")
+print()
+
+print (Pyt5)
+Odp5 = input("Prawda czy fałsz: ")
+if Odp5 == "Prawda":
+    print("Dobra odpowiedź!")
+else:
+    print("Zła odpowiedź!")
+print()
+
+print (Pyt6)
+Odp6 = input("Prawda czy fałsz: ")
+if Odp6 == "Fałsz":
+    print("Dobra odpowiedź!")
+else:
+    print("Zła odpowiedź!")
+print()
+
+# print (Pyt7)
+# print ("Prawda czy fałsz?")
+#
+# print (Pyt8)
+# print ("Prawda czy fałsz?")
+#
+# print (Pyt9)
+# print ("Prawda czy fałsz?")
+#
+# print (Pyt0)
+# print ("Prawda czy fałsz?")
+
+# print ()
+# print ("Dziękuje za grę")
+# print ("Liczba uzyskanych punktów: ")
+# print (Wynik )
